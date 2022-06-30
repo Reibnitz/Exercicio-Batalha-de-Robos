c5e01c087ae23a03991924e25196f4bdc52a6cb3
Reibnitz <lucas.fpolis@gmail.com>
2022-06-30 17:02:33 -0300

    Tratamento de erros para número de rodadas

--- Projeto/Program.cs
+++ Projeto/Program.cs
@@ -9,34 +9,42 @@ RoboFactory factory = new();
 Console.WriteLine("########## Bem-vindo a Batalha de Robôs ##########");
 
 Console.WriteLine("Escolha o tipo do primeiro competidor (leve / pesado):");
-string? tipo1 = Convert.ToString(Console.ReadLine());
+string? tipo1 = Console.ReadLine();
 
 Console.WriteLine("Escolha o nome do primeiro competidor:");
-string? nome1 = Convert.ToString(Console.ReadLine());
+string? nome1 = Console.ReadLine();
 
 Console.WriteLine("Escolha o tipo do segundo competidor (leve / pesado):");
-string? tipo2 = Convert.ToString(Console.ReadLine());
+string? tipo2 = Console.ReadLine();
 
 Console.WriteLine("Escolha o nome do segundo competidor:");
-string? nome2 = Convert.ToString(Console.ReadLine());
+string? nome2 = Console.ReadLine();
 
 RespostaService<IRobo> robo1 = factory.MakeRobo(tipo1, nome1);
 RespostaService<IRobo> robo2 = factory.MakeRobo(tipo2, nome2);
 
 Console.WriteLine("Defina o número de rodadas:");
-int rodadas = Convert.ToInt32(Console.ReadLine());
 
-if (robo1.Sucesso && robo2.Sucesso)
+try
 {
-    Console.WriteLine("Pressione ENTER para começar a batalha!");
-    Console.ReadLine();
-    Console.Clear();
+    int rodadas = Convert.ToInt32(Console.ReadLine());
 
-    PartidaBatalha.Batalhar(robo1, robo2, rodadas);
+    if (robo1.Sucesso && robo2.Sucesso)
+    {
+        Console.WriteLine("Pressione ENTER para começar a batalha!");
+        Console.ReadLine();
+        Console.Clear();
+
+        PartidaBatalha.Batalhar(robo1, robo2, rodadas);
+    }
+
+    else
+    {
+        if (!robo1.Sucesso) Console.WriteLine(robo1.MensagemErro);
+        if (!robo2.Sucesso) Console.WriteLine(robo2.MensagemErro);
+    }
 }
-
-else
+catch (Exception)
 {
-    if (!robo1.Sucesso) Console.WriteLine(robo1.MensagemErro);
-    if (!robo2.Sucesso) Console.WriteLine(robo2.MensagemErro);
+    Console.WriteLine("Número de rodadas inválido. Utilize apenas números");
 }
